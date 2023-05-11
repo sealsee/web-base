cd0a81eb98fa93bbbf984e53de3d4af05cd59a1f
zero-zero-7 <402844629@qq.com>
2023-05-11 14:27:04 +0800

    adjust

--- public/basemodel/baseEntity.go
+++ public/basemodel/baseEntity.go
@@ -3,17 +3,10 @@ package basemodel
 import "github.com/sealsee/web-base/public/ds/page"
 
 type BaseEntity struct {
-<<<<<<< HEAD
-	CreateBy   *int64    `json:"createBy"`   //创建人
-	CreateTime *BaseTime `json:"createTime"` //创建时间
-	UpdateBy   *int64    `json:"updateBy"`   //修改人
-	UpdateTime *BaseTime `json:"updateTime"` //修改时间
-=======
 	CreateBy   *int64    `json:"createBy,omitempty" db:"create_by"`     //创建人
 	CreateTime *BaseTime `json:"createTime,omitempty" db:"create_time"` //创建时间
 	UpdateBy   *int64    `json:"updateBy,omitempty" db:"update_by"`     //修改人
 	UpdateTime *BaseTime `json:"updateTime,omitempty" db:"update_time"` //修改时间
->>>>>>> 43344ce9b7156bec5d55f5ff4eb92b91991ebd5a
 }
 
 type BaseEntityQuery struct {
