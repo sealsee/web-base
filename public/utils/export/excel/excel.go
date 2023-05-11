766bb5e84e3ae6e391cbcc33de50fa4e94785698
liker <liker365@163.com>
2023-05-11 17:43:35 +0800

    excel数据转为接口类型，服务端省去转换

--- public/utils/export/excel/excel.go
+++ public/utils/export/excel/excel.go
@@ -6,21 +6,14 @@ import (
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
 
-func ExportExcel(rows [][]string) (data []byte) {
-	//https://xuri.me/excelize/zh-hans/sheet.html#DuplicateRow
-	// 创建一个工作表
-	//index := f.NewSheet("Sheet1")
-	//var a = make([]string, 0, 1)
-	// 设置单元格的值
-	//f.SetSheetRow("Sheet1", "1", &a)
-	//
-	//f.SetCellValue("Sheet1", "A2", a)
-	// 设置工作簿的默认工作表
-	//f.SetActiveSheet(index)
-
+func ExportExcel(dataList [][]interface{}) (data []byte) {
 	f := excelize.NewFile()
-	for i, row := range rows {
-		f.SetSheetRow("Sheet1", "A"+strconv.Itoa(i+1), &row)
+	for i, row := range dataList {
+		if i == 0 {
+			f.SetSheetRow("Sheet1", "A1", &row)
+		} else {
+			f.SetSheetRow("Sheet1", "A"+strconv.Itoa(i+1), &row)
+		}
 	}
 	buffer, _ := f.WriteToBuffer()
 	return buffer.Bytes()
