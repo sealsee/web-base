f8296b9c7f2017149fc05cf09098878877f5c037
liker <liker365@163.com>
2023-05-12 15:24:14 +0800

    gorm自定义数据类型须实现Scanner/Valuer接口

--- public/basemodel/baseTime.go
+++ public/basemodel/baseTime.go
@@ -1,6 +1,7 @@
 package basemodel
 
 import (
+	"database/sql/driver"
 	"fmt"
 	"time"
 )
@@ -24,4 +25,18 @@ func (t BaseTime) MarshalJSON() ([]byte, error) {
 
 func (t BaseTime) String() string {
 	return time.Time(t).Format(timeFormat)
+}
+
+// gorm自定义数据类型须实现Scanner/Valuer接口
+func (t BaseTime) Value() (driver.Value, error) {
+	return time.Time(t).Format(timeFormat), nil
+}
+
+func (t *BaseTime) Scan(v interface{}) error {
+	value, ok := v.(time.Time)
+	if ok {
+		*t = BaseTime(value)
+		return nil
+	}
+	return fmt.Errorf("can not convert %v to timestamp", v)
 }
