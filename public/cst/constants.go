706c0fb4098b343049adcf5269e8a00269ba91f0
liker <liker365@163.com>
2023-05-26 17:05:47 +0800

    加个常量

--- public/cst/constants.go
+++ public/cst/constants.go
@@ -9,3 +9,8 @@ const (
 	UploadMaxNum             = 50               // 多文件上传最多支持50个文件
 	UploadMaxSize      int64 = 10 * 1024 * 1024 // 单文件最大支持10MB
 )
+
+const (
+	USER_TYPE_SYS = "00" // 系统分配用户（B端）
+	USER_TYPE_REG = "01" // 注册用户（C端）
+)
