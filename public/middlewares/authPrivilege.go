ed9e46e133759d6110c99cdb4fdcc5404eb13863
zero-zero-7 <402844629@qq.com>
2023-06-06 16:20:44 +0800

    privilege

---
+++ public/middlewares/authPrivilege.go
@@ -0,0 +1,41 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sealsee/web-base/public/context"
+	"github.com/sealsee/web-base/public/errs"
+	"github.com/sealsee/web-base/public/web"
+)
+
+var cp ICheckPrivilege
+
+type ICheckPrivilege interface {
+	check(path string, loginUser *context.SessionUser) bool
+}
+
+func SetCheckPrivilege(check ICheckPrivilege) {
+	cp = check
+}
+
+func PrivilegeMiddleware() func(c *gin.Context) {
+	return func(c *gin.Context) {
+		path := c.Request.URL.Path
+		if path == "" || path == "/" {
+			return
+		}
+
+		user := context.NewUserContext(c).GetUser()
+		if cp == nil {
+			return
+		}
+		if cp.check(path, user) {
+			c.Next()
+		} else {
+			json := web.NewJsonResult(c)
+			json.SetErrs(errs.REFUSE_VISIT_ERR).Render()
+			c.Abort()
+			return
+		}
+	}
+
+}
